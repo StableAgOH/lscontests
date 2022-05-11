97c051f8fe7eaa6581a0b879cf2222eb64605303
StableAgOH <stagoh17@gmail.com>
2022-05-11 12:56:21 +0800

    perf: sort contests start time by default

--- src/cli.ts
+++ src/cli.ts
@@ -9,6 +9,7 @@ program
     .version(version)
     .option("-d, --days, <day>", "Number of days to get contests information", "3")
     .option("-l, --list", "List all supported OJ")
+    .option("--no-sort", "Do not sort by contests start time (default sort by OJ)")
     .addOption(new Option("-o, --oj <ojs...>", "OJs to get contests information").choices(Object.keys(alloj)))
     .parse();
 
@@ -16,7 +17,7 @@ const opts = program.opts();
 
 async function main() {
     if (opts.list) console.log(Object.values(alloj).map((oj) => oj.name));
-    else console.log(await list(opts.oj, opts.days as number));
+    else console.log(await list(opts.oj, opts.days as number, opts.sort));
 }
 
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { addOJ, alloj } from "./lib/oj";
 
-async function list(abbrList?: string[], days = 3) {
+async function list(abbrList?: string[], days = 3, sort = true) {
     if (!abbrList) abbrList = Object.keys(alloj);
-    return (await Promise.all(
+    const contests = (await Promise.all(
         abbrList.map(
             async (abbr) => (await alloj[abbr].get()).filter(
                 (ct) => ct.startTime <= new Date(Date.now() + (days as number) * 86400000)
             )
         )
     )).reduce((ls1, ls2) => ls1.concat(ls2));
+    if (sort) contests.sort((a, b) => a.startTime.getTime() - b.startTime.getTime());
+    return contests;
 }
 
 export { addOJ, list };
