f35d42d42f19057778f040b35285f1df57b9b0e1
StableAgOH <stagoh17@gmail.com>
2022-05-11 15:42:58 +0800

    fix: throw an error when codeforces API errors

--- src/lib/oj/codeforces.ts
+++ src/lib/oj/codeforces.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 import { contest, rule } from "../contest";
 import { oj } from ".";
 
@@ -21,7 +22,12 @@ const ruleRecord: Record<string, rule> = {
 export const cf: oj = {
     name: "Codeforces",
     get: async () => {
-        const resList: result[] = (await axios.get("https://codeforces.com/api/contest.list")).data.result;
+        const response = await axios.get("https://codeforces.com/api/contest.list");
+        if (!(response.data instanceof Object)) {
+            const $ = cheerio.load(response.data);
+            throw new Error($.text());
+        }
+        const resList: result[] = response.data.result;
         return resList.map((res): contest => {
             return {
                 ojName: cf.name,
