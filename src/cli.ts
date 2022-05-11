8f358ab2ef5d3e6e9ac499939f9343de396e621a
StableAgOH <stagoh17@gmail.com>
2022-05-12 00:16:44 +0800

    perf: allows to specify the program name when calling cli()

--- src/cli.ts
+++ src/cli.ts
@@ -4,18 +4,18 @@ import { Option, program } from "commander";
 import { alloj } from "./lib/oj";
 import { config, getContestList, getContestInfo, langList } from ".";
 
-program
-    .name("lsct")
-    .version(version)
-    .option("-d, --days, <day>", "Number of days to get contests information", "3")
-    .option("-l, --list", "List all supported OJ")
-    .addOption(new Option("-o, --oj <ojs...>", "OJs to get contests information").choices(Object.keys(alloj)))
-    .addOption(new Option("-r, --raw", "Print raw contest list").conflicts("language"))
-    .addOption(new Option("-L, --language <lang>", "Set output language").default("zh-CN").choices(langList))
-    .option("--no-sort", "Do not sort by contests start time, but by OJ order");
+export async function cli(arg?: string, name?: string) {
+    program
+        .name(name ? name : "lsct")
+        .version(version)
+        .option("-d, --days, <day>", "Number of days to get contests information", "3")
+        .option("-l, --list", "List all supported OJ")
+        .addOption(new Option("-o, --oj <ojs...>", "OJs to get contests information").choices(Object.keys(alloj)))
+        .addOption(new Option("-r, --raw", "Print raw contest list").conflicts("language"))
+        .addOption(new Option("-L, --language <lang>", "Set output language").default("zh-CN").choices(langList))
+        .option("--no-sort", "Do not sort by contests start time, but by OJ order")
+        .parse(arg ? [...process.argv.slice(0, 2), ...arg.split(" ")] : process.argv);
 
-export async function cli(arg?: string) {
-    program.parse(arg ? [...process.argv.slice(0, 2), ...arg.split(" ")] : process.argv);
     const opts = program.opts();
     if (opts.list) return Object.values(alloj).map((oj) => oj.name).join("\n");
     else {
