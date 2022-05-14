6c4e14dff6c46176f9e395f742413ff215259e70
StableAgOH <stagoh17@gmail.com>
2022-05-15 01:16:29 +0800

    ci: additional validation

--- release/verify.js
+++ release/verify.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 
 function main()
 {
+    if(!fs.existsSync("./dist/cli.js"))
+    {
+        console.error("Cannot find ./dist/cli.js, did you forget to build?");
+        return 1;
+    }
     if(!fs.existsSync("README.md"))
     {
         console.error("There is no README.md");
