6cd7b0308a6c0f36902febf3590f9dcbdf5f1374
StableAgOH <stagoh17@gmail.com>
2022-05-15 01:13:07 +0800

    ci: enhance logic

--- release/verify.js
+++ release/verify.js
@@ -7,6 +7,16 @@ function main()
         console.error("There is no README.md");
         return 1;
     }
+    if(!fs.existsSync("README-zh-CN.md"))
+    {
+        console.error("There is no README-zh-CN.md");
+        return 1;
+    }
+    if(!fs.existsSync("README-zh-NE.md"))
+    {
+        console.error("There is no README-zh-NE.md");
+        return 1;
+    }
     return 0;
 }
 
